859146704c8c45dcb3d541470791c07225356d8c
grupoimpactacc <78936317+grupoimpactacc@users.noreply.github.com>
2021-05-26 14:15:58 -0300

    Add files via upload

---
+++ Exercício 21/src/tad_lista_nodos/exception/InvalidPositionException.java
@@ -0,0 +1,5 @@
+package tad_lista_nodos.exception;
+@SuppressWarnings("serial")
+public class InvalidPositionException extends RuntimeException {
+	public InvalidPositionException(String err) {super(err);}
+}
---
+++ Exercício 21/src/tad_mapa/exceptions/EmptyPriorityQueueException.java
@@ -0,0 +1,7 @@
+package tad_mapa.exceptions;
+
+public class EmptyPriorityQueueException extends RuntimeException {
+	public EmptyPriorityQueueException(String err){
+		super(err);
+	}
+}
